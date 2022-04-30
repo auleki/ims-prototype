07c1a304ec53f078cbdca31eded67419012577bf
Emmanuel B. Giwa <kodagiwa@gmail.com>
2022-04-30 01:18:56 +0100

    add login page

---
+++ components/styled.ts
@@ -0,0 +1,116 @@
+import styled, { css } from "styled-components"
+import { colors, metrics } from "../utils/constants"
+
+
+export const SFullPage = styled.div(
+    () => css`
+        height: 100vh;
+        background: ${colors.black};
+        overflow: hidden;
+        color: ${colors.white};
+    `
+)
+
+export const SAuthPage = styled.div(
+    () => css`
+        background: url('https://res.cloudinary.com/dyj6pqx6d/image/upload/v1651275005/ims-prototype/pexels-sam-kolder-2387873_hr7jxd.jpg');
+        background-size: cover;
+        height: 100vh;
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    `
+)
+
+export const SContainer = styled.div(
+    () => css`
+        display: flex;
+        justify-center: center;
+        align-items: center;
+        /* outline: 2px solid crimson; */
+    `
+    )
+
+export const SPage = styled.div(
+    () => css`
+        background: ${colors.black};
+        color: ${colors.white};
+    `
+)
+
+export const SInput = styled.input(
+    () => css`
+        background: ${colors.lightBlack};
+        padding: 1rem 2rem;
+        font-size: ${metrics.normalFont};
+        outline: 0;
+        color: ${colors.white};
+        border: 0;
+        border-radius: ${metrics.borderRadius};
+        &::placeholder {
+            color: ${colors.white};
+        }
+    `
+)
+
+export const SFormContainer = styled.div(
+    () => css`
+        background-color: ${colors.black};
+        color: ${colors.white};
+        display: inline-flex;
+        /* height: 350px; */
+        justify-content: flex-end;
+        gap: 1rem;
+        border-radius: ${metrics.borderRadius};
+        width: 400px;
+        padding: 3rem;
+        flex-direction: column;
+    `
+)
+
+export const SButton = styled.button(
+    () => css`
+        padding: 1rem 2rem;
+        border: none;
+        background: ${colors.white};
+        transition: background 150ms ease-out, transform 100ms ease-in;
+        border-radius: ${metrics.buttonRadius};
+        font-size: ${metrics.normalFont};
+        &:hover {
+            cursor: pointer;
+            background: ${colors.red};
+            color: ${colors.white};
+        }
+
+        &:active {
+            transform: translateY(.3rem);
+        }
+    `
+)
+export const SCenteredIcon = styled.span(
+    () => css`
+        font-size: ${metrics.bigFont};
+        /* text-align: center; */
+        height: 50px;
+        width: 50px;
+        text-align: center;
+        background: ${colors.lightBlack};
+        display: inline;
+        border-radius: ${metrics.buttonRadius};
+    `
+)
+
+export const SCenteredText = styled.h2(
+    () => css`
+        font-size: ${metrics.mediumFont};
+        text-align: center;
+    `
+)
+
+export const SRow = styled.div(
+    () => css`
+        width: 100%;
+    `
+)
---
+++ utils/constants.ts
@@ -0,0 +1,23 @@
+import * as RiIcon from 'react-icons/ri'
+
+export const colors = {
+    black: "#1a1a1a",
+    lightBlack: "#505050",
+    white: "#ffffff",
+    red: "#ff2b2b",
+}
+
+export const fonts = {}
+
+export const metrics = {
+    borderRadius: ".5rem",
+    buttonRadius: "100px",
+    bigFont: "2rem",
+    mediumFont: "1.5rem",
+    normalFont: "1.2rem",
+    smallFont: "1rem"
+}
+
+export const icons = {
+    user: RiIcon.RiUser6Fill,
+}
